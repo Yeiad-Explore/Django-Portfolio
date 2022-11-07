cdcdfe1d3f97e1807382c3f5d9c4dabdf7acfb48
yeiad <kabidyeiad101@gmail.com>
2022-11-07 22:56:31 +0600

    Second Commit

--- new/models.py
+++ new/models.py
@@ -6,3 +6,6 @@ class Contact(models.Model):
     name = models.CharField(max_length = 100,null= True)
     email = models.EmailField(max_length = 100,null= True)
     message = models.TextField(max_length = 100,null= True)
+
+    def __str__(self):
+        return self.name
--- new/views.py
+++ new/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 def home(request):
@@ -10,5 +11,19 @@ def about(request):
 
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        contact = Contact()
+        contact.name = name
+        contact.email = email
+        contact.message = message
+        contact.save()
     return render(request,'contact.html')
 
+def showdata(request):
+    contacts = Contact.objects.all()
+    print(type(contacts))
+    data = {'Contact': contacts}
+    return render(request,'showdata.html',data)
